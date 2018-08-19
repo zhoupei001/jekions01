2d0bd933a8592b8765eb99e8315f95b40b6c8997
zhoupei001 <42488545+zhoupei001@users.noreply.github.com>
2018-08-19 10:21:03 +0800

    Update test000.py

--- YAML/test000.py
+++ YAML/test000.py
@@ -5,7 +5,7 @@ class Test01():
     @allure.step('我是测试步骤001')
     def test001(self):
         print("001被执行")
-        assert 1
+       
 
     @pytest.allure.severity(pytest.allure.severity_level.CRITICAL)
     def test002(self):
